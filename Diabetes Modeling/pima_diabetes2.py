2d937259b0affed21e8e9828e1900cfbc1cee072
Aditya Kuroodi <akuroodi@gmail.com>
2020-12-11 09:32:27 -0800

    Added Keras optimizer sweep

--- Diabetes Modeling/pima_diabetes2.py
+++ Diabetes Modeling/pima_diabetes2.py
@@ -38,9 +38,20 @@ y = dataset[:,-1]        # all rows but just with last col
 
 
 
-# Define and compile a Keras model using KerasClassifier
+# Define and compile Keras models using KerasClassifier
 
-def pimaModel():
+def pimaModel_model_sweep(optimizer='adam'):
+    model = Sequential();
+    model.add(Dense(12, activation="relu", input_dim=8))     # 12 nodes with 8 input cols
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1,activation='sigmoid'))
+
+    # NOTE: compile model with configurable optimzer to sweep through available choices in Keras
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def pimaModel_batch_epoch_sweep():
     model = Sequential()
     model.add(Dense(12, activation="relu", input_dim=8))     # 12 nodes in first hidden layer, first layer needs 8 since 8 inputs
     model.add(Dense(8, activation='relu'))
@@ -48,12 +59,16 @@ def pimaModel():
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
-model = KerasClassifier(build_fn=pimaModel, verbose=0)
+# NOTE: Use to build model for evaluating batch/epoch sizes
+# model = KerasClassifier(build_fn=pimaModel_model_sweep, verbose=0)
+# batch_size = [20, 40, 60, 80, 100]
+# epochs = [10, 50, 100]
+# param_grid = dict(batch_size=batch_size, epochs=epochs)
 
-# Define grid search parameters
-batch_size = [20, 40, 100]
-epochs = [10, 5, 10]
-param_grid = dict(batch_size=batch_size, epochs=epochs)
+#NOTE: Use to build model for evaluating different optimizer algorithms 
+model = KerasClassifier(build_fn=pimaModel_model_sweep, batch_size=20, epochs=100, verbose=0)
+optimizers = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+param_grid = dict(optimizer=optimizers)
 
 # cv parameter is to determine # of folds in K-fold, n_jobs=1 is default single core operation
 grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
@@ -65,4 +80,4 @@ means = grid_result.cv_results_['mean_test_score']
 stds = grid_result.cv_results_['std_test_score']
 params = grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
-    print("Mean accyracy %f with deviation (%f) with: %r" % (mean, stdev, param))
+    print("Mean accuracy %f with deviation (%f) with: %r" % (mean, stdev, param))
